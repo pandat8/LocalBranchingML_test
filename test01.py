e2806eb8b5324f20beb6a22a205eaf582a335c35
Defeng <pandat8@gmail.com>
2020-12-05 17:22:32 -0500

    test

--- test01.py
+++ test01.py
@@ -12,10 +12,24 @@ status = pyscipopt_model.getStatus()
 print("* Model status: %s" % status)
 if status == 'optimal':
     print("Optimal value:", pyscipopt_model.getObjVal())
-    for v in pyscipopt_model.getVars():
-        if v.name != "n":
-            print("%s: %d" % (v, pyscipopt_model.getVal(v)))
+    # for v in pyscipopt_model.getVars():
+    #     if v.name != "n":
+    #         print("%s: %d" % (v, pyscipopt_model.getVal(v)))
 else:
     print("* No variable is printed if model status is not optimal")
+
+model = Model("MILP")
+
+x = model.addVar("x")
+y = model.addVar("y", vtype="INTEGER")
+model.setObjective(x + y)
+model.addCons(2*x - y*y >= 0)
+model.optimize()
+sol = model.getBestSol()
+print("Model MILP is solved ")
+print("x: {}".format(sol[x]))
+print("y: {}".format(sol[y]))
+
+
 
 
